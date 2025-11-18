96e5be68f65a915fbaffc34bb4968a6e11b0d6ce
EvyatarG12 <graidy@post.bgu.ac.il>
2025-11-18 14:28:37 +0200

    add is_prime function

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 print("Git practice")
 print("Yossi Balabuski")
+
+print("Hello World")
+
+def is_prime(x):
+    if x<=2:
+        return True
+    for i in range(2 , x):
+        if x%i == 0:
+            return False
+    return True
+
+print(is_prime(997))
